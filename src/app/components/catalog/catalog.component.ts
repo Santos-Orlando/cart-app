c46a8e814d8ae7fdf4135e658d050a2773e0cc03
PessoaNormal <pessoa.promedio@gmail.com>
2024-10-07 17:35:37 +0100

    :construction: Add addProductEventEmitter attribute

--- src/app/components/catalog/catalog.component.ts
+++ src/app/components/catalog/catalog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductCardComponent } from '../product-card/product-card.component';
 
@@ -11,4 +11,11 @@ import { ProductCardComponent } from '../product-card/product-card.component';
 export class CatalogComponent {
 
   @Input() products! : Product[];
+
+  @Output() addProductEventEmitter : EventEmitter<Product> = new EventEmitter();
+
+  addProduct(product : Product){
+    this.addProductEventEmitter.emit(product);
+  }
+ 
 }
