f566e333acb40398982b7bf6351cc63ed5aaf82b
PessoaNormal <pessoa.promedio@gmail.com>
2024-10-09 13:20:06 +0100

    :construction: Create total attribute and calculateTotal method

--- src/app/components/cart-app/cart-app.component.ts
+++ src/app/components/cart-app/cart-app.component.ts
@@ -14,12 +14,16 @@ import { CartItem } from '../../models/cartitem';
 export class CartAppComponent implements OnInit{
 
   products : Product[] = [];
+
   items: CartItem[] = [];
+
+  total: number = 0; 
 
   constructor(private service : ProductService){}
   
   ngOnInit(): void {
-    this.products = this.service.findAll();  
+    this.products = this.service.findAll();
+    this.calculateTotal();  
   }
 
   addProduct(product : Product){
@@ -34,9 +38,15 @@ export class CartAppComponent implements OnInit{
     }else{
       this.items = [...this.items, {product: {...product}, quantity: 1}];
     }
+    this.calculateTotal(); 
   }
 
   onDeleteCart(id: number): void{
     this.items = this.items.filter(item => item.product.id != id);
+    this.calculateTotal(); 
+  }
+
+  calculateTotal(): void{
+    this.total = this.items.reduce((accumulator, item) => accumulator + (item.quantity * item.product.price), 0)
   }
 }
