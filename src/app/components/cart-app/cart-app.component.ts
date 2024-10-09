dd0b310177dec5ae5603165be6012e2604cff576
PessoaNormal <pessoa.promedio@gmail.com>
2024-10-09 17:18:28 +0100

    :construction: Add cart-modal to cart-app component

--- src/app/components/cart-app/cart-app.component.ts
+++ src/app/components/cart-app/cart-app.component.ts
@@ -5,11 +5,12 @@ import { CatalogComponent } from "../catalog/catalog.component";
 import { CartComponent } from '../cart/cart.component';
 import { CartItem } from '../../models/cartitem';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { CartModalComponent } from '../cart-modal/cart-modal.component';
 
 @Component({
   selector: 'cart-app',
   standalone: true,
-  imports: [CatalogComponent, CartComponent, NavbarComponent],
+  imports: [CatalogComponent, NavbarComponent, CartModalComponent],
   templateUrl: './cart-app.component.html'
 })
 export class CartAppComponent implements OnInit{
@@ -61,7 +62,7 @@ export class CartAppComponent implements OnInit{
     sessionStorage.setItem('cart', JSON.stringify(this.items));
   }
 
-  openCart(): void{
+  openCloseCart(): void{
     this.showCart = !this.showCart;
   }
 }
