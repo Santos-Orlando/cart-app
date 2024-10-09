f19872165779756e468428bc403dce0b34f3e037
PessoaNormal <pessoa.promedio@gmail.com>
2024-10-09 16:00:46 +0100

    :construction: Create navbar

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CartItem } from '../../models/cartitem';
+
+@Component({
+  selector: 'navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html'
+})
+export class NavbarComponent {
+
+  @Input() items!: CartItem[];
+
+  @Output() openEventEmitter : EventEmitter<boolean> = new EventEmitter();
+
+  openCart(): void{
+    this.openEventEmitter.emit();
+  }
+}
