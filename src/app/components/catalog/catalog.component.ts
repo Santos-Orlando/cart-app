bd09e6ec158968f425d33b1707dc3c9d70371051
PessoaNormal <pessoa.promedio@gmail.com>
2024-10-11 22:09:11 +0100

    :recycle: Use store to load the products

--- src/app/components/catalog/catalog.component.ts
+++ src/app/components/catalog/catalog.component.ts
@@ -3,6 +3,8 @@ import { Product } from '../../models/product';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { SharingDataService } from '../../services/sharing-data.service';
 import { ProductService } from '../../services/product.service';
+import { Store } from '@ngrx/store';
+import { load } from '../../store/products.actions';
 
 @Component({
   selector: 'catalog',
@@ -14,11 +16,14 @@ export class CatalogComponent implements OnInit{
 
   products! : Product[];
 
-  constructor(private productService: ProductService,
-              private sharingData: SharingDataService){}
+  constructor(private store: Store<{products: any}>,
+              private productService : ProductService,
+              private sharingData: SharingDataService){
+                this.store.select('products').subscribe(state => this.products = state.products);
+              }
 
   ngOnInit(): void {
-    this.products = this.productService.findAll();
+    this.store.dispatch(load({products: this.productService.findAll()}));
   }
 
   addProduct(product : Product){
