830954e7457382f3548e14d3f761f22bc026fa5d
PessoaNormal <pessoa.promedio@gmail.com>
2024-10-11 10:52:58 +0100

    :construction: Inject router to the constructor

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { CartItem } from '../../models/cartitem';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'cart',
@@ -14,7 +15,12 @@ export class CartComponent {
   total: number = 0;
   
   idProductEventEmitter : EventEmitter<number> = new EventEmitter();
-  
+
+  constructor(private router: Router){
+    this.items = this.router.getCurrentNavigation()?.extras.state!['items'];
+    this.total = this.router.getCurrentNavigation()?.extras.state!['total'];
+  }
+
   onDeleteCart(id: number){
     this.idProductEventEmitter.emit(id);
   }
