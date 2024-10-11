a56b94d93e460b24667db5ede4eb4dba018bf3da
PessoaNormal <pessoa.promedio@gmail.com>
2024-10-11 21:29:10 +0100

    :wrench: Install ngrx/devtools package

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { itemReducer } from './store/items.reducer';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), provideStore({
-      items: itemReducer
-    })]
+        items: itemReducer
+    }), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
 };
