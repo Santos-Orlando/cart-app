03b1b54b4828465d475b3c5b32bb90e83afadda7
PessoaNormal <pessoa.promedio@gmail.com>
2024-10-11 19:21:21 +0100

    :recycle: Correct items reducer

--- src/app/store/items.reducer.ts
+++ src/app/store/items.reducer.ts
@@ -9,40 +9,43 @@ export interface ItemState {
 
 export const initialState: ItemState = {
     items: JSON.parse(sessionStorage.getItem('cart') || '[]'),
-    total: 0
+    total: 0,
 }
 
 export const itemReducer = createReducer(
     initialState,
-    on(add, (state, {product}) => {
+    on(add, (state, { product }) => {
         const hasItem = state.items.find((item: CartItem) => item.product.id === product.id);
-      if(hasItem){
-        return {
-            items: state.items = state.items.map((item: CartItem) => {
-                if(item.product.id == product.id){
-                    return {...item, quantity: item.quantity + 1}
-                }
-                return item;
-            }),
-            total: state.total
+        if (hasItem) {
+            return {
+                items: state.items.map((item: CartItem) => {
+                    if (item.product.id === product.id) {
+                        return {
+                            ...item,
+                            quantity: item.quantity + 1
+                        }
+                    }
+                    return item;
+                }),
+                total: state.total
             }
-        
-      }else{
-        return {
-            items: [...state.items, {product: {...product}, quantity: 1}],
-            total: state.total};
+        } else {
+            return {
+                items: [...state.items, { product: { ...product }, quantity: 1 }],
+                total: state.total
+            };
         }
     }),
-    on(remove, (state, {id})=> {
+    on(remove, (state, { id }) => {
         return {
-            items: state.items.filter(item => item.product.id != id),
+            items: state.items.filter(item => item.product.id !== id),
             total: state.total
         }
     }),
     on(total, state => {
         return {
-            items: state.items, 
-            total: state.items.reduce((accumulator, item) => accumulator + (item.quantity * item.product.price), 0)
+            items: state.items,
+            total: state.items.reduce((accumulator, item) => accumulator + item.quantity * item.product.price, 0)
         }
     })
 )
