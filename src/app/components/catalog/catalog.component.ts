8501a54b14d73ee285c46f3a463723faeb532be1
PessoaNormal <pessoa.promedio@gmail.com>
2024-10-11 12:20:42 +0100

    :recycle: Get product array from ProductService

--- src/app/components/catalog/catalog.component.ts
+++ src/app/components/catalog/catalog.component.ts
@@ -1,8 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductCardComponent } from '../product-card/product-card.component';
-import { Router } from '@angular/router';
 import { SharingDataService } from '../../services/sharing-data.service';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'catalog',
@@ -10,12 +10,15 @@ import { SharingDataService } from '../../services/sharing-data.service';
   imports: [ProductCardComponent],
   templateUrl: './catalog.component.html'
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit{
 
   products! : Product[];
 
-  constructor(private sharingData: SharingDataService, private router: Router){
-    this.products = this.router.getCurrentNavigation()?.extras.state!['products'];
+  constructor(private productService: ProductService,
+              private sharingData: SharingDataService){}
+
+  ngOnInit(): void {
+    this.products = this.productService.findAll();
   }
 
   addProduct(product : Product){
