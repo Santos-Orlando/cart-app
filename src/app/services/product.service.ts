1b3cbd21c23bf4264a7c54172e8c5068a305c136
orlando-santos <santos.orlando.martin@gmail.com>
2024-12-01 15:46:32 +0000

    :recycle: Use products list got from backend

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product';
-import { products } from '../data/product.data';
+import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private baseUrl = 'http://localhost:8080'; // URL del backend Spring Boot
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  findAll() : Observable<Product[]>{
-    return of(products);
+  findAll(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.baseUrl}/api/products`); // Especifica directamente el tipo
   }
+
 }
