9e62cec378c5a01e7de3d509214582cf80d1bd5c
orlando-santos <santos.orlando.martin@gmail.com>
2024-12-01 15:48:12 +0000

    :sparkles: Render and format image base64 in card

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,19 +1,23 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Product } from '../../models/product';
 
 @Component({
   selector: 'div[product-card]',
   standalone: true,
   imports: [],
+  styleUrl: './product-card.component.css',
   templateUrl: './product-card.component.html'
 })
 export class ProductCardComponent {
-
+  
   @Input() product! : Product;
+  @Input() base64Image: string = '';
 
   @Output() addProductEventEmitter : EventEmitter<Product> = new EventEmitter();
 
   onAddCart(product:Product){
     this.addProductEventEmitter.emit(product);
   }
+  
+  
 }
